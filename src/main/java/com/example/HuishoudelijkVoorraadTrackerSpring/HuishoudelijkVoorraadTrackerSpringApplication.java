8076b5be67c1811aa7a18aca4f5fafa7bf3ccc0c
Ayak <alihan26262626@gmail.com>
2021-06-04 13:01:41 +0200

    h18 testing initial controller

--- src/main/java/com/example/HuishoudelijkVoorraadTrackerSpring/HuishoudelijkVoorraadTrackerSpringApplication.java
+++ src/main/java/com/example/HuishoudelijkVoorraadTrackerSpring/HuishoudelijkVoorraadTrackerSpringApplication.java
@@ -2,9 +2,20 @@ package com.example.HuishoudelijkVoorraadTrackerSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class HuishoudelijkVoorraadTrackerSpringApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home(){
+		return "hello world";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HuishoudelijkVoorraadTrackerSpringApplication.class, args);
 	}
